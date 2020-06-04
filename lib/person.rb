f2d484f0d0c04ffc993517814a682b5758f0f712
temorymatine <65198626+temorymatine@users.noreply.github.com>
2020-06-04 18:10:45 -0400

    first commit

--- lib/person.rb
+++ lib/person.rb
@@ -2,22 +2,50 @@
 require 'pry'
 
 class Person
-    attr_reader  #getters
-    attr_writer #setters
-    attr_accessor :bank_account, :name, :happiness_score, :hygiene_score
+    attr_reader :name,:happiness #getters
+    attr_writer 
+    attr_accessor  :bank_account, :hygiene
 
-    def initialize (name, bank_account=25, happiness_score=8, hygiene_score=8)
+    def initialize (name, bank_account=25, happiness=8 , hygiene=8)
         @name = name
         @bank_account = bank_account
-        @happiness_score = happiness_score
-        @hygiene_score = hygiene_score
-        # binding.pry
+        @happiness = happiness
+        @hygiene = hygiene
+
+        
+        # if @happiness > 10 
+        #     @happiness = 10
+        # end 
+
+        #binding.pry
+    end
+
+    def happiness=(happiness)
+        if happiness > 10 
+        @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else 
+            @happiness = happiness
+        end 
+    end
+    def hygiene=(hygiene)
+        if hygiene > 10 
+            @hygiene = 10
+            elsif hygiene < 0
+                @hygiene = 0
+            else 
+                @hygiene = hygiene
+            end 
     end
 
     def clean?
+        
+    @hygiene > 7 
     end
 
     def happy?
+        
     end
 
     def get_paid
@@ -35,11 +63,11 @@ class Person
     def start_conversation
     end
 
-    #Each person has a name, bank_account (starting at $25), happiness_score, hygiene_score
-        # happiness_score min=0, max=10, start=8
-        # hygiene_score min=0, max=10, start=8
+    #Each person has a name, bank_account (starting at $25), happiness, hygiene
+        # happiness min=0, max=10, start=8
+        # hygiene min=0, max=10, start=8
 
 end
 
+
 mike = Person.new("Mike")
-binding.pry
