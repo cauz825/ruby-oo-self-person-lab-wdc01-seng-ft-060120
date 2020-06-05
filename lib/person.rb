519bc4bd4d881e3fb4f5953068b417fb2e8fad14
MCauz <michaelhcausey@gmail.com>
2020-06-04 20:49:34 -0400

    after dinner attempt

--- lib/person.rb
+++ lib/person.rb
@@ -2,36 +2,30 @@
 require 'pry'
 
 class Person
-    attr_reader :name,:happiness #getters
+    attr_reader :name,:happiness, :hygiene #getters
     attr_writer 
-    attr_accessor  :bank_account, :hygiene
+    attr_accessor  :bank_account
 
     def initialize (name, bank_account=25, happiness=8 , hygiene=8)
         @name = name
         @bank_account = bank_account
         @happiness = happiness
         @hygiene = hygiene
-
-        
-        # if @happiness > 10 
-        #     @happiness = 10
-        # end 
-
-        #binding.pry
     end
 
     def happiness=(happiness)
         if happiness > 10 
-        @happiness = 10
+            @happiness = 10
         elsif happiness < 0
             @happiness = 0
         else 
             @happiness = happiness
         end 
     end
+
     def hygiene=(hygiene)
         if hygiene > 10 
-            @hygiene = 10
+                @hygiene = 10
             elsif hygiene < 0
                 @hygiene = 0
             else 
@@ -39,33 +33,27 @@ class Person
             end 
     end
 
-    def clean?
-        
-    @hygiene > 7 
-    end
+def clean?
+    hygiene > 7
+end
 
-    def happy?
-        
-    end
+def happy?
+    happiness > 7
+end
 
-    def get_paid
-    end
+def get_paid(salary)
+    @bank_account = @bank_account + salary
+    return "all about the benjamins"
+end
 
-    def take_bath
-    end
+def take_bath
+    @hygiene = @hygiene + 4
+    hygiene=(hygiene)
+    binding.pry
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+end
 
-    def work_out
-    end
-
-    def call_friend
-    end
-
-    def start_conversation
-    end
-
-    #Each person has a name, bank_account (starting at $25), happiness, hygiene
-        # happiness min=0, max=10, start=8
-        # hygiene min=0, max=10, start=8
+    
 
 end
 
